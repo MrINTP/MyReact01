3675bef97665a2777d20feea7050fbc98a036a91
MrINTP <gon809846@gmail.com>
2023-11-01 13:58:44 +0900

    오늘 수업 ㅋ

--- src/App.js
+++ src/App.js
@@ -22,25 +22,50 @@
 //   );
 // }
 
-// export default App;
-
-import "./App.css";
-import React, { Component } from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import MyMain from "./mymain/MyMain";
-import MyTest from "./mytest/MyTest";
+import MyNotFound from "./mynotfound/MyNotFound";
 import Myfile01 from "./mycomponent/Myfile01";
 import MyHeader from "./myheader/MyHeader";
+import "./App.css";
+import MyTest from "./mytest/MyTest";
 
 const App = () => {
-  return (
+  return(
     <div className="App">
-      <Routes>
-        <MyHeader />
-        <Route path="/" element={<MyMain />} />
-      </Routes>
+      <BrowserRouter>
+      <MyHeader />
+        <Routes>
+          <Route path="/" element={<MyMain />}/>
+          <Route path="file" element={<Myfile01/>}/>
+          <Route path="test" element={<MyTest/>}/>
+          {/*Other routes are not found*/}
+          <Route path="*" element={<MyNotFound />}/>
+        </Routes>
+      
+      </BrowserRouter>
     </div>
   );
-};
-
+}
 export default App;
+
+// import React from "react";
+
+// const App = () => {
+
+//   const isToggled = true;
+
+//   return(
+//     <div>
+//       {/* 스타일 style (변수 사용을 위함){(스타일 객체) {}객체} => 암튼 중괄호를 두번 써야함*/}
+//       {/* 자바스크립트의 변수값으로만 DOM을 제어할 수 있음. 이에 접근 최소화를 할 수 있다. 리엑트에서는 지양한다. */}
+//       style={{
+//         //?화살표에서 true면 red, false면 blue
+//         color: isToggled ? "red" : "blue"
+//       }}안녕하세요 안녕하세요
+//     </div>
+//   );
+
+// }
+// export default App;
--- src/mymain/MyMain.js
+++ src/mymain/MyMain.js
@@ -1,6 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 
 function MyMain() {
+
+  const [isToggled, setIsToggled] = useState(true);
+//  let isToggled = false;
+
+  const btnChangeColor = () => {
+    setIsToggled(!isToggled);
+  }
+  
   return (
     <>
       <div>
@@ -10,6 +18,13 @@ function MyMain() {
         <div>
           <p>나는 나는 메인입니다. 흐헤헤</p>
         </div>
+       {/* 스타일 style (변수 사용을 위함){(스타일 객체) {}객체} => 암튼 중괄호를 두번 써야함*/}
+       {/* 자바스크립트의 변수값으로만 DOM을 제어할 수 있음. 이에 접근 최소화를 할 수 있다. 리엑트에서는 지양한다. */}
+       <p style={{color: isToggled ? "blue" : "red"}} >
+       안녕하세요 안녕하세요
+       </p>
+       <button onClick={btnChangeColor}>색상 변경</button>
+
       </div>
     </>
   );
