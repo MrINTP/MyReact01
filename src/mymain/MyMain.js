9b56f26978bedd4f75fe194058655f8d7d2fdc2d
MrINTP <gon809846@gmail.com>
2023-11-01 14:55:45 +0900

    아 쓰기 귀찮아

--- src/mymain/MyMain.js
+++ src/mymain/MyMain.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 function MyMain() {
 
@@ -8,6 +9,17 @@ function MyMain() {
   const btnChangeColor = () => {
     setIsToggled(!isToggled);
   }
+
+  //js에서 다른 페이지로 넘어가는 방법!
+  const navFilePage = () => {
+    // window.location.href 쓰면 또 뭉탱이를 가져오기 떄문에 useNavigate를 사용한다.
+    // window.location.href = "/file";
+
+    gayJoyGo('/file');
+    //게이조이고 변수는 useNavigate를 쓰며, 사용 방법은 존나 간단함.
+  }
+
+  const gayJoyGo = useNavigate();
   
   return (
     <>
@@ -24,6 +36,20 @@ function MyMain() {
        안녕하세요 안녕하세요 zzzz
        </p>
        <button onClick={btnChangeColor}>색상 변경</button>
+       <div>
+        <p>이것은 Link to로 사용했을 경우입니다.</p>
+        <Link to="/file"> <button>File로 이동</button> </Link>
+       </div>
+       <div>
+        <p>이것은 a태그 사용했을 경우(비추, 사유는 bundles.js(뭉탱이)를 계속 다운받아 비효율적임)</p>
+        <a href="/file"><button>File로 이동</button> </a>
+       </div>
+       <div>
+        {/* js에서 다른 페이지로 넘어가는 방법 적용하기 ㅋ */}
+        {/* 하지만 이를 그대로 쓰면 뭉탱이를 또 가져오는 문제가 있음 해결 방법은 useNavigate를 사용함 */}
+        <button onClick={navFilePage}>화살표 함수 버튼</button>
+       </div>
+       
 
       </div>
     </>
